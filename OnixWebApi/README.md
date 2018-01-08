be612296990957da8c3dbef6bc22cf1239c60f0b
Kuba Buda <j.buda@onix.pl>
2018-01-08 11:12:56 +0100

    InitialMigration
    DB Context reading and adding works

---
+++ OnixWebApi/README.md
@@ -0,0 +1,14 @@
+﻿* Getting started
+
+Install postgresql (tested on Postgres 10)
+Run:
+dotnet restore
+Set proper ConnectionString in appsettings.json
+Run:
+dotnet ef database update 
+
+* DB Models changes
+
+This is code first EF Core application, 
+After updating models run:
+dotnet ef database update 
--- OnixWebApi/Services/HeroService.cs
+++ OnixWebApi/Services/HeroService.cs
@@ -9,40 +9,21 @@ namespace OnixWebApi.Services
 {
     public class HeroService
     {
-        IEnumerable<Hero> Heroes = new List<Hero>()
-        {
-            new Hero() { Id = 11, Name = "Mr. Nice" },
-            new Hero() { Id = 12, Name = "Narco" },
-            new Hero() { Id = 13, Name = "Bombasto" },
-            new Hero() { Id = 14, Name = "Celeritas" },
-            new Hero() { Id = 15, Name = "Magneta" },
-            new Hero() { Id = 16, Name = "RubberMan" },
-            new Hero() { Id = 17, Name = "Dynama" },
-            new Hero() { Id = 18, Name = "Dr IQ" },
-            new Hero() { Id = 19, Name = "Magma" },
-            new Hero() { Id = 20, Name = "Tornado" }
-        };
+        private OrderShippingContext _context;
 
-        //private OrderShippingContext _context;
-
-        public HeroService() //OrderShippingContext dbContextFunc)
+        public HeroService(OrderShippingContext dbContextFunc)
         {
-            //_context = dbContextFunc;
+            _context = dbContextFunc;
         }
 
         public IEnumerable<Hero> GetHeroes()
         {
-            //using (var context = _dbContextFunc())
-            //{ _context.
-            var heroes = Heroes.ToList();
-            //}
-
-            return Heroes;
+            return _context.Heroes.ToList();
         }
 
         public Hero GetHero(int id)
         {
-            return Heroes.Where(hero => id == hero.Id).Single();
+            return _context.Heroes.Where(hero => id == hero.Id).Single();
         }
     }
 }

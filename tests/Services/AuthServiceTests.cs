923a067245ce434dfd6f2ece703f0ae0a12f27e9
Kuba Buda <j.buda@onix.pl>
2018-06-07 15:59:29 +0200

    EOD, adding tests using Entity

---
+++ tests/Services/AuthServiceTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WebApi.Services;
+using WebApi.Services.Interfaces;
+
+namespace WebApiTests.Services
+{
+    public class AuthServiceTests
+    {
+        IAuthorizeService _authorizeService;
+
+        [SetUp]
+        public void Setup()
+        {
+
+            //_authorizeService = new AuthService();
+        }
+
+        [Test]
+        public void IsValidPassword()
+        {
+        }
+    }
+}

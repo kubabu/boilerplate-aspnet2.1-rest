f862473e4fb33a9dca9f26a52777dbf27afa7402
Kuba Buda <j.buda@onix.pl>
2018-06-07 15:01:39 +0200

    Add unit tests

--- WebApi/Controllers/VersionInfoController.cs
+++ WebApi/Controllers/VersionInfoController.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
 namespace WebApi.Controllers
 {
+    [Produces("application/json")]
     [Route("api/[controller]")]
     public class VersionInfoController : Controller
     {
@@ -20,35 +22,11 @@ namespace WebApi.Controllers
 
         // GET api/values
         [HttpGet]
+        [AllowAnonymous]
         public IEnumerable<string> Get()
         {
             _logger.LogWarning("Version info called");
-            return new string[] { string.Format("Git hash: {0}", Program.GitHash()) };
+            return new string[] { string.Format("version hash: {0}", Program.GitHash()) };
         }
-
-        //// GET api/values/5
-        //[HttpGet("{id}")]
-        //public string Get(int id)
-        //{
-        //    return "value";
-        //}
-
-        //// POST api/values
-        //[HttpPost]
-        //public void Post([FromBody]string value)
-        //{
-        //}
-
-        //// PUT api/values/5
-        //[HttpPut("{id}")]
-        //public void Put(int id, [FromBody]string value)
-        //{
-        //}
-
-        //// DELETE api/values/5
-        //[HttpDelete("{id}")]
-        //public void Delete(int id)
-        //{
-        //}
     }
 }

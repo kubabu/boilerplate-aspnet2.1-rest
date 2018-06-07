c9ffb4fa31d38bb6c259f92804fd171e7a2e9ca6
Kuba Buda <j.buda@onix.pl>
2018-06-07 15:45:51 +0200

    Unit tests for password check service

---
+++ tests/Services/CheckPasswordServiceTests.cs
@@ -0,0 +1,42 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WebApi.Models;
+using WebApi.Services;
+using WebApi.Services.Interfaces;
+
+namespace WebApiTests.Services
+{
+    public class CheckPasswordServiceTests
+    {
+        private ICheckPasswordService _service;
+        string _pass;
+        string _hashed;
+
+        [SetUp]
+        public void Setup()
+        {
+            _service = new CheckPasswordService();
+            _pass = "FooBar 123";
+            _hashed = _service.HashPassword(_pass);
+        }
+
+        [Test]
+        public void IsValidPassword()
+        {
+            Assert.True(_service.IsValidPassword(_pass, _hashed));
+        }
+
+        [Test]
+        public void IsValidPasswordForUser()
+        {
+            var user = new User();
+            Assert.True(_service.IsValidPassword(_pass, _hashed));
+            user.Password = _hashed;
+
+            Assert.True(_service.IsPasswordValidForUser(user, _pass));
+        }
+    }
+}

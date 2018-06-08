5634a7ab9715e2f476d8478038201c604b09443f
Kuba Buda <j.buda@onix.pl>
2018-06-08 14:12:51 +0200

    VS builds, detects and launches tests

--- tests/Services/AuthServiceTests.cs
+++ tests/Services/AuthServiceTests.cs
@@ -1,7 +1,12 @@
-﻿using NUnit.Framework;
+﻿using Microsoft.EntityFrameworkCore;
+using Moq;
+using NUnit.Framework;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using WebApi.Models;
+using WebApi.Models.DbContexts;
 using WebApi.Services;
 using WebApi.Services.Interfaces;
 
@@ -9,18 +14,51 @@ namespace WebApiTests.Services
 {
     public class AuthServiceTests
     {
+        string _username;
+        bool _ifPass;
+        Mock<ICheckPasswordService> _passwordService;
         IAuthorizeService _authorizeService;
 
         [SetUp]
         public void Setup()
         {
+            _username = "FooBar";
+            var users = new List<User>()
+            {
+                new User() {
+                    Name = _username
+                },
+            };
+            var set = new Mock<DbSet<User>>();
+            Mock<MainDbContext> dbMock = new Mock<MainDbContext>(Mock.Of< DbContextOptions<MainDbContext>>()); //dbContext, ICheckPasswordService checkPasswordService
 
-            //_authorizeService = new AuthService();
+            _passwordService = new Mock<ICheckPasswordService>();
+            _passwordService.Setup(p => p.IsPasswordValidForUser(It.IsAny<User>(), It.IsAny<string>()))
+                .Returns(_ifPass);
+            _authorizeService = new AuthService(dbMock.Object, _passwordService.Object);
         }
 
         [Test]
-        public void IsValidPassword()
+        public async Task IsValidPassword_invalid()
         {
+            // setup
+            _ifPass = false;
+            // act
+            var result = await _authorizeService.AuthorizeWithLoginAndPasswordAsync(_username, "invalid password");
+            // verify
+            Assert.Null(result);
+        }
+
+        [Test]
+        public async Task IsValidPassword_valid()
+        {
+            // setup
+            _ifPass = true;
+            // act
+            var result = await _authorizeService.AuthorizeWithLoginAndPasswordAsync(_username, "valid password");
+            // verify
+            Assert.NotNull(result);
+            Assert.Equals(result.Name, _username);
         }
     }
 }

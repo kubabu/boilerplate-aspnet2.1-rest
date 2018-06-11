762738412df32e17d1a5785f25eb4bc00f2efa22
Kuba Buda <j.buda@onix.pl>
2018-06-11 15:00:15 +0200

    Testing EF Core using SQLite for DbContext, works

--- tests/Services/AuthServiceTests.cs
+++ tests/Services/AuthServiceTests.cs
@@ -1,64 +1,75 @@
-﻿//using Microsoft.EntityFrameworkCore;
-//using Moq;
-//using NUnit.Framework;
-//using System;
-//using System.Collections.Generic;
-//using System.Text;
-//using System.Threading.Tasks;
-//using WebApi.Models;
-//using WebApi.Models.DbContexts;
-//using WebApi.Services;
-//using WebApi.Services.Interfaces;
+﻿using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using WebApi.Models;
+using WebApi.Models.DbContexts;
+using WebApi.Services;
+using WebApi.Services.Interfaces;
 
-//namespace WebApiTests.Services
-//{
-//    public class AuthServiceTests
-//    {
-//        string _username;
-//        bool _ifPass;
-//        Mock<ICheckPasswordService> _passwordService;
-//        IAuthorizeService _authorizeService;
+namespace WebApiTests.Services
+{
+    public class AuthServiceTests
+    {
+        string _username = "FooBar";
+        bool _ifPass;
+        MainDbContext _dbContext;
+        IAuthorizeService _authorizeService;
 
-//        [SetUp]
-//        public void Setup()
-//        {
-//            _username = "FooBar";
-//            var users = new List<User>()
-//            {
-//                new User() {
-//                    Name = _username
-//                },
-//            };
-//            var set = new Mock<DbSet<User>>();
-//            Mock<MainDbContext> dbMock = new Mock<MainDbContext>(Mock.Of< DbContextOptions<MainDbContext>>()); //dbContext, ICheckPasswordService checkPasswordService
+        [SetUp]
+        public void Setup()
+        {
+            var connection = new SqliteConnection("DataSource=:memory:");
+            connection.Open();
 
-//            _passwordService = new Mock<ICheckPasswordService>();
-//            _passwordService.Setup(p => p.IsPasswordValidForUser(It.IsAny<User>(), It.IsAny<string>()))
-//                .Returns(_ifPass);
-//            _authorizeService = new AuthService(dbMock.Object, _passwordService.Object);
-//        }
+            var options = new DbContextOptionsBuilder<MainDbContext>()
+                .UseSqlite(connection)
+                .Options;
+            _dbContext = new MainDbContext(options);
+            
+            // Create the schema in the database
+            using (var context = new MainDbContext(options))
+            {
+                context.Database.EnsureCreated();
+            }
 
-//        [Test]
-//        public async Task IsValidPassword_invalid()
-//        {
-//            // setup
-//            _ifPass = false;
-//            // act
-//            var result = await _authorizeService.AuthorizeWithLoginAndPasswordAsync(_username, "invalid password");
-//            // verify
-//            Assert.Null(result);
-//        }
+            var passwordService = new Mock<ICheckPasswordService>();
+            passwordService.Setup(p => p.IsPasswordValidForUser(It.IsAny<User>(), It.IsAny<string>()))
+                .Returns(() => this._ifPass);
 
-//        [Test]
-//        public async Task IsValidPassword_valid()
-//        {
-//            // setup
-//            _ifPass = true;
-//            // act
-//            var result = await _authorizeService.AuthorizeWithLoginAndPasswordAsync(_username, "valid password");
-//            // verify
-//            Assert.NotNull(result);
-//            Assert.Equals(result.Name, _username);
-//        }
-//    }
-//}
+            _authorizeService = new AuthService(_dbContext, passwordService.Object, Mock.Of<ILogger<AuthService>>());
+        }
+
+        [Test]
+        public async Task IsValidPassword_invalid()
+        {
+            // setup
+            _ifPass = false;
+            // act
+            var result = await _authorizeService.AuthorizeWithLoginAndPasswordAsync(_username, "invalid password");
+            // verify
+            Assert.Null(result);
+        }
+
+        [Test]
+        public async Task IsValidPassword_valid()
+        {
+            // setup
+            _ifPass = true;
+            _dbContext.Users.Add(new User() { Name = _username });
+            await _dbContext.SaveChangesAsync();
+            int usersCount = await _dbContext.Users.CountAsync();
+            Assert.AreEqual(1, usersCount);
+            // act
+            var result = await _authorizeService.AuthorizeWithLoginAndPasswordAsync(_username, "valid password");
+            // verify
+            Assert.NotNull(result);
+            Assert.AreEqual(result.Name, _username);
+        }
+    }
+}
